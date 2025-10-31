66aa20d8b9d2dabd3bb6bbcaca6890e410f101ca
Woohyeok Baek <qordngur156@gmail.com>
2025-10-31 16:16:10 +0900

    shadcn 설정

---
+++ src/widgets/header/Header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <div className="flex justify-between items-center p-4">
+      <div className="flex items-center gap-4">
+        <div className="text-lg font-bold">Venture Capital</div>
+        <div>메뉴바</div>
+      </div>
+      <div>유저</div>
+    </div>
+  );
+}
