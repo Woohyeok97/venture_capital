25ff6f6eb4e3b109372d296eb026c22a44edc8a9
Woohyeok Baek <qordngur156@gmail.com>
2025-11-26 16:50:24 +0900

    chore: 로딩UI 스타일 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,11 +2,11 @@ import { Spinner } from '@/shared/ui/spinner/spinner';
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 bg-black/40 flex justify-center items-center h-screen">
-      <div className="flex gap-4">
-        <Spinner className="size-12 text-purple-300" />
-        <Spinner className="size-12 text-purple-500" />
-        <Spinner className="size-12 text-purple-300" />
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
+      <div className="flex items-center gap-4 animate-pulse">
+        <Spinner className="size-10 text-purple-400 drop-shadow-[0_0_10px_rgba(168,85,247,0.7)]" />
+        <Spinner className="size-16 text-purple-500 drop-shadow-[0_0_14px_rgba(139,92,246,0.8)] mt-4" />
+        <Spinner className="size-12 text-purple-400 drop-shadow-[0_0_10px_rgba(168,85,247,0.7)]" />
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return <div>Home</div>;
-  // redirect('/browse/investments');
+  // return <div>Home</div>;
+  redirect('/browse/investments');
 }
